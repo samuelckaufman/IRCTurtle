3b7a849448e232fac1ea33397da6a4a3128e4385
Samuel Kaufman <samuel.c.kaufman@gmail.com>
2011-04-09 20:03:38 +0000

    launcher

--- lib/IRCTurtle.pm
+++ lib/IRCTurtle.pm
@@ -1,5 +1,52 @@
-use strict;
-use warnings;
 package IRCTurtle;
+use Moo;
+use IO::Async::Loop;
+use Net::Async::IRC;
 
+has loop => (
+    is => 'ro',
+    lazy => 1,
+    default => sub { IO::Async::Loop->new }
+);
+has irc => (
+    is => 'ro',
+    builder => '_build_irc',
+    lazy => 1
+);
+has nick => (
+    is => 'ro',
+    required => 1,
+);
+has host => (
+    is => 'ro',
+    required => 1,
+);
+
+has channel => (
+    is => 'ro',
+    required => 1,
+);
+
+sub _build_irc {
+    my $self = shift;
+    my $irc = Net::Async::IRC->new(
+        on_message_text => sub {
+            my ( $self, $message, $hints ) = @_;
+            print "$hints->{prefix_name} says: $hints->{text}\n";
+        }
+    );
+    $self->loop->add($irc);
+    $irc;
+}
+sub launch {
+    my $self = shift;
+    $self->irc->login(
+        nick => $self->nick,
+        host => $self->host,
+        on_login => sub {
+            $self->irc->send_message( "JOIN", undef, $self->channel );
+        },
+    );
+    $self->loop->loop_forever;
+}
 1;
---
+++ script/launch.pl
@@ -0,0 +1,21 @@
+use strictures 1;
+use FindBin;
+use lib "$FindBin::Bin/../lib";
+use IRCTurtle;
+use Getopt::Long::Descriptive;
+my ( $opt, $usage ) = describe_options(
+    'my-program %o <some-arg>',
+    [ 'host|h=s', "the host to connect to" ],
+    [ 'nick|n=s', "nick,alias" ],
+    [ 'channel|c=s', "channel to join" ],
+    [ 'help',      "print usage message and exit" ],
+);
+
+print( $usage->text ), exit if $opt->help;
+
+my $turtle = IRCTurtle->new(
+    host    => $opt->host,
+    nick    => $opt->nick,
+    channel => $opt->channel,
+);
+$turtle->launch;
